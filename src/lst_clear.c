6041c5eb0cfee724043169166ebe014de860c8a7
gsistelos <gabrielsistelos@gmail.com>
2024-09-04 19:39:49 -0300

    lst_clear and tests

---
+++ src/lst_clear.c
@@ -0,0 +1,19 @@
+#include "liblst.h"
+
+#include <stdlib.h>
+
+void lst_clear(t_lst *lst, void (*free_func)(void *)) {
+    t_node *node = lst->head;
+
+    while (node) {
+        t_node *next = node->next;
+
+        free_func(node->data);
+        free(node);
+
+        node = next;
+    }
+
+    lst->head = NULL;
+    lst->tail = NULL;
+}
---
+++ tests/src/lst_clear_test.c
@@ -0,0 +1,17 @@
+#include "liblst.h"
+
+#include <criterion/criterion.h>
+
+Test(lst_clear_test, clear) {
+    t_lst lst;
+    lst_init(&lst);
+
+    lst_push_back(&lst, strdup("Hello, world!"));
+    lst_push_back(&lst, strdup("Hello, again!"));
+    lst_push_back(&lst, strdup("Hello, world, again!"));
+
+    lst_clear(&lst, free);
+
+    cr_assert_eq(lst.head, NULL);
+    cr_assert_eq(lst.tail, NULL);
+}
